a80703742624747b9bb9c8284d5c16fad864f143
Alan MacLeod <amacleod@rskorbital.com>
2015-10-17 15:41:43 +0100

    floor and ceilings texture mapped. begin splitting rendering of vertical (wall) and horizontal(floor) planes into two separate jobs

---
+++ util/Player.js
@@ -0,0 +1,15 @@
+/**
+ * Created by alan on 17/10/15.
+ */
+
+
+function Player(x, y, z, v, height)
+{
+    this.x = 0 || x;
+    this.y = 0 || y;
+    this.z = 0 || z;
+    this.v = 0 || v;
+    this.pitch = 0;
+    this.height = 25 || height;
+    this.sector = 0;
+}
